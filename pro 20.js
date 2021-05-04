46b7f80a931d7707b3e4593aa882bc677f9fabdf
sanayadav <82660493+sanayadav@users.noreply.github.com>
2021-05-04 18:09:39 +0530

    Add files via upload

---
+++ pro 20.js
@@ -0,0 +1,17 @@
+var car, wall ;
+var speed, weight ;
+
+
+function setup(){
+speed=random(55,90);
+weight=random(400,1500);
+
+var car = createSprite(50,200,50,50);
+}
+
+
+
+function draw(){
+drawSprites();
+
+}
